e36149c57b68c0ba332af178b50666745aebb72d
Reinhard Stahn <ReinhardStahn@t-online.de>
2019-06-07 11:26:53 +0200

    Fix PolynomialFeatures

--- src/polynomial-features.ts
+++ src/polynomial-features.ts
@@ -8,22 +8,15 @@ export class PolynomialFeatures {
   }
 
   transform(x: number[][]): number[][] {
-    if (this.include_bias) {
-      for (let xi of x) {
-        xi.push(1);
-      }
-    }
-
     let y: number[][] = [];
     for (let xi of x) {
       let yi: number[] = [];
-      console.log(xi);
-      for (let comb of combinations_with_replacement(xi, this.degree)) {
+      let ximod = this.include_bias ? xi.concat([1]) : xi.concat();
+      for (let comb of combinations_with_replacement(ximod, this.degree)) {
         yi.push(comb.reduce((p,c) => p*=c, 1));
       }
       y.push(yi)
     }
-
     return y;
   }
 
