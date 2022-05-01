4ec6acf2b139b98d47dc4f5f95b1e0d3c479df2e
Rainij <Rainij@users.noreply.github.com>
2022-05-01 08:22:07 +0000

    deploy: 335004a70af9385a95c9f0ab738810ee1ce2cd26

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { PolynomialRegressor } from './polynomial-regression'
-export { PolynomialFeatures } from './polynomial-features'
-export { RegressionError } from './util/util'
--- src/util/util.ts
+++
@@ -1,1 +0,0 @@
-export class RegressionError extends Error {}
