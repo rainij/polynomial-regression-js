86914ec94cfd819a207b91dd96940bde0455dbb4
Reinhard Stahn <51020828+rainij@users.noreply.github.com>
2022-08-10 19:22:16 +0200

    Refactor testing of itertools (#9)
    Using jests parameterized tests instead of awkward self baked
    solution. This exposed a minor bug which we fixed hereby.

--- src/util/itertools.test.ts
+++ src/util/itertools.test.ts
@@ -1,125 +1,120 @@
 import { combinations, combinationsWithRepitition } from './itertools.js'
 
-/// Auxiliary Stuff
 
-/**
- * Testing of itertools functions combinations and combinationsWithRepition
- */
-class CombiTester {
-  private _combinations: <T>(iterable: Iterable<T>, degree: number) => IterableIterator<T[]>;;
+interface TestCase {
+  readonly k: number,
+  readonly pool: readonly number[],
+  readonly desiredResult: readonly (readonly number[])[],
+};
 
-  constructor(combi: <T>(iterable: Iterable<T>, degree: number) => IterableIterator<T[]>) {
-    this._combinations = combi;
-  }
 
-  // Execute the actual test:
-  // Expect to get all k-sets (with/without repitition) from pool
-  execute(pool: number[], k: number, desiredResult: number[][]): void {
-    let result: number[][] = [];
-
-    for (const comb of this._combinations(pool, k)) {
-      result.push([...comb]); // spread operator helps creating copy
-    }
-
-    expect(result).toStrictEqual(desiredResult);
-  }
-}
-
-/// Actual Tests
-
-describe('Tests for itertools', () => {
-  describe('Tests for choosing k of n without repitition', () => {
-    const combiTester = new CombiTester(combinations);
-
-    it('Choose 2 of 4', () => {
-      const k = 2
-      const pool = [1, 2, 3, 4];
-      const desiredResult = [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 1 of 5', () => {
-      const k = 1
-      const pool = [1, 2, 3, 4, 5];
-      const desiredResult = [[1], [2], [3], [4], [5]];
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 3 of 3', () => {
-      const k = 3
-      const pool = [1, 2, 3];
-      const desiredResult = [[1, 2, 3]];
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 0', () => {
-      const k = 0
-      const pools = [[], [1], [1, 2], [1, 2, 3]];
-      const desiredResult = [[]];
-
-      for (const pool of pools) {
-        combiTester.execute(pool, k, desiredResult);
-      }
-    });
-
-    it('Choose less than 0 or more than n', () => {
-      // TODO in this case probably good to throw error. Fractions should be
-      // forbidden too.
+describe('combinations: choose k of n without repitition', () => {
+  describe('valid arguments', () => {
+    it.each([
+      {
+        k: 0,
+        pool: [],
+        desiredResult: [[]],
+      },
+      {
+        k: 0,
+        pool: [1],
+        desiredResult: [[]],
+      },
+      {
+        k: 0,
+        pool: [1, 2, 3],
+        desiredResult: [[]],
+      },
+      {
+        k: 1,
+        pool: [1, 2, 3, 4, 5],
+        desiredResult: [[1], [2], [3], [4], [5]],
+      },
+      {
+        k: 2,
+        pool: [1, 2, 3, 4],
+        desiredResult: [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]],
+      },
+      {
+        k: 3,
+        pool: [1, 2, 3],
+        desiredResult: [[1, 2, 3]],
+      },
+      {
+        k: 3,
+        pool: [1, 2, 3, 4],
+        desiredResult: [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]],
+      },
+    ])('case no. %#', ({k, pool, desiredResult}: TestCase) => {
+      const actualResult: number[][] = [...combinations(pool, k)];
+      expect(actualResult).toEqual(desiredResult);
     });
   });
 
-  describe('Tests for choosing k of n with repitition', () => {
-    const combiTester = new CombiTester(combinationsWithRepitition);
-
-    it('Choose 2 of 4', () => {
-      const k = 2
-      const pool = [1, 2, 3, 4];
-      const desiredResult = [[1,1], [2,1], [3,1], [4,1], [2,2], [3,2], [4,2], [3,3], [4,3], [4,4]]
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 1 of 6', () => {
-      const k = 1
-      const pool = [1, 2, 3, 4, 5, 6];
-      const desiredResult = [[1], [2], [3], [4], [5], [6]];
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 3 of 3', () => {
-      const k = 3
-      const pool = [1, 2, 3];
-      const desiredResult = [[1, 1, 1], [2, 1, 1], [3, 1, 1], [2, 2, 1], [3, 2, 1], [3, 3, 1], [2, 2, 2], [3, 2, 2],
-        [3, 3, 2], [3, 3, 3]];
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 3 of 2', () => {
-      const k = 3
-      const pool = [1, 2];
-      const desiredResult = [[1, 1, 1], [2, 1, 1], [2, 2, 1], [2, 2, 2]];
-
-      combiTester.execute(pool, k, desiredResult);
-    });
-
-    it('Choose 0', () => {
-      const k = 0
-      const pools = [[], [1], [1, 2], [1, 2, 3]];
-      const desiredResult = [[]];
-
-      for (const pool of pools) {
-        combiTester.execute(pool, k, desiredResult);
-      }
-    });
-
-    it('Choose less than 0', () => {
-      // TODO in this case probably good to throw error. Fractions should be
-      // forbidden too.
+  describe('invalid arguments', () => {
+    it.each([
+      -1,  // must be >= 0
+      1.3, // must be integer
+      3,   // must be <= pool-size
+    ])('choose k = %j', (k: number) => {
+      expect(() => [...combinations([1, 2], k)]).toThrow();
     });
   });
-})
+});
+
+
+describe('combinationsWithRepitition: choose k of n with repitition', () => {
+  describe('valid arguments', () => {
+    it.each([
+      {
+        k: 0,
+        pool: [],
+        desiredResult: [[]],
+      },
+      {
+        k: 0,
+        pool: [1],
+        desiredResult: [[]],
+      },
+      {
+        k: 0,
+        pool: [1, 2, 3],
+        desiredResult: [[]],
+      },
+      {
+        k: 1,
+        pool: [1, 2, 3, 4, 5, 6],
+        desiredResult: [[1], [2], [3], [4], [5], [6]],
+      },
+      {
+        k: 3,
+        pool: [1, 2, 3],
+        desiredResult: [
+          [1, 1, 1], [2, 1, 1], [3, 1, 1], [2, 2, 1], [3, 2, 1], [3, 3, 1],
+          [2, 2, 2], [3, 2, 2], [3, 3, 2], [3, 3, 3]
+        ],
+      },
+      {
+        k: 3,
+        pool: [1, 2],
+        desiredResult: [
+          [1, 1, 1], [2, 1, 1], [2, 2, 1], [2, 2, 2]
+        ],
+      },
+    ])('case no. %#', ({k, pool, desiredResult}: TestCase) => {
+      const actualResult: number[][] = [...combinationsWithRepitition(pool, k)];
+      expect(actualResult).toEqual(desiredResult);
+    });
+  });
+
+  // TODO test on invalid input,
+  describe('invalid arguments', () => {
+    it.each([
+      -1,  // must be >= 0
+      1.3, // must be integer
+    ])('choose k = %j', (k: number) => {
+      expect(() => [...combinationsWithRepitition([1, 2], k)]).toThrow();
+    });
+  });
+});
--- src/util/itertools.ts
+++ src/util/itertools.ts
@@ -1,5 +1,8 @@
 /** Basic combinatorial utility. For internal use only. */
 
+// TODO the two functions are inconsistent with respect to the order in which
+// they produce the combinations. This should be corrected.
+
 /**
  * Yield all k-combinations with repition from a given sequence.
  *
@@ -20,34 +23,33 @@
  * @param k The length of the combinations: 1, 2, 3, ...
  */
 export function* combinationsWithRepitition<T>(iterable: Iterable<T>, k: number) {
-  if (k < 0) return [];
-
   const pool = Array.from(iterable);
-  const maxi = pool.length-1;
+  const max_index = pool.length - 1;
 
   if (k < 0 || k % 1 !== 0) {
-    throw new Error("k must be a non-negative integer.")
+    throw new Error("k must be a non-negative integer.");
   }
 
-  if (k > 0 && pool[0] === undefined) {
-    throw new Error("If k is non-zero, iterable is not allowed to be empty.")
+  if (k > 0 && pool.length === 0) {
+    throw new Error("If k is non-zero, iterable is not allowed to be empty.");
   }
 
   const indices: number[] = Array.from(Array(k), () => 0);
-  const next: T[] = indices.map(() => pool[0]!);
+  let next: T[] = indices.map(i => pool[i]);
 
   yield next;
 
   outerLoop:
   while (true) {
     let n = 0;
-    while (indices[n] === maxi) { ++n; }
+    while (indices[n] === max_index) { ++n; }
     if (n === k) break outerLoop;
     const current = indices[n] += 1;
-    next[n] = pool[current]!;
+    next = [...next]; // *shallow* copy
+    next[n] = pool[current];
     while (--n >= 0) {
       indices[n] = current;
-      next[n] = pool[current]!;
+      next[n] = pool[current];
     }
     yield next;
   }
@@ -81,25 +83,27 @@ export function* combinationsWithRepitition<T>(iterable: Iterable<T>, k: number)
  * @param k The length of the combinations: 1, 2, 3, ...
  */
 export function* combinations<T>(iterable: Iterable<T>, k: number) {
+  // TODO it is not really helpfull to have an iterable as input
   const pool = Array.from(iterable);
 
   if (k > pool.length || k < 0 || k % 1 !== 0) {
-    throw new Error("k must be in {0, 1, 2, ..., n}, where n is the length of the iterable.")
+    throw new Error("k must be in {0, 1, 2, ..., n}, where n is the length of the iterable.");
   }
 
   const indices: number[] = Array.from({length: k}, (_, i) => i);
-  const next: T[] = indices.map(i => pool[i]!);
+  let next: T[] = indices.map(i => pool[i]);
   yield next;
 
   while (true) {
-    let n = k-1;
+    let n = k - 1;
     while (indices[n] === pool.length - k + n) { --n; };
     if (n === -1) break;
     ++indices[n];
-    next[n] = pool[indices[n]!]!;
+    next = [...next]; // *shallow* copy
+    next[n] = pool[indices[n]];
     while (++n < k) {
-      indices[n] = indices[n-1]! + 1;
-      next[n] = pool[indices[n]!]!;
+      indices[n] = indices[n-1] + 1;
+      next[n] = pool[indices[n]];
     }
     yield next;
   }
