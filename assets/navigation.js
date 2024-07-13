7b0b52b4e4f8d1931d6be4f47ef36524553430f5
rainij <rainij@users.noreply.github.com>
2024-07-13 12:30:39 +0000

    deploy: 9ec463ac935cc3975ed7616f88fdf8dfdfd0ecdf

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA42QTwvCMAxHv0vORXGgSK+iZ/EqHsrMtmKXShJhQ/zuHvyHWym7P97LL8c7KHYKFvYx9BRb78IOnd4YBQxcnTZgoQxOBGU+ZmaNtgEMXDydwS6K9cMkjAesGUUi55RfKOd8Qz7SljnlGwDT7vus2USqfP2TelLkypXJ6S/4P1AsV/kHTCoM6HHi9ARz0MEVtwEAAA=="
